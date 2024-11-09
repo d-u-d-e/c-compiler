e1f9e0afadcf2db855a2039e7192eba208494f11
Alberto Ursino <albertoursino98@gmail.com>
2024-11-09 17:59:35 +0100

    refactor: update docs, remove node id

--- ast/tree.py
+++ ast/tree.py
@@ -1,54 +1,80 @@
-from tree_node import TreeNode
+from tree_node import NodeType, TreeNode
+
 
 class Tree:
     def __init__(self):
-        self._nodes = {}
-        self._root = None
+        self._nodes: set[TreeNode] = []
+        self._root: TreeNode = None
 
-    def __contains__(self, identifier):
-        return identifier in self._nodes
-    
-    def __getitem__(self, key) -> TreeNode:
-        try:
-            return self._nodes[key]
-        except KeyError:
-            raise Exception(f"Node with id {key} is not in the tree")
-        
-    def __len__(self):
+    def __contains__(self, node: TreeNode) -> bool:
+        return node in self._nodes
+
+    def __getitem__(self, node: TreeNode) -> TreeNode:
+        if node not in self._nodes:
+            raise KeyError(f"Node '{node}' is not in the tree")
+        return node
+
+    def __len__(self) -> int:
         return len(self._nodes)
 
-    def add_node(self, node, parent=None):
-        # sanity checks
-        if not isinstance(node, TreeNode):
-            raise Exception("'node' must be an instance of 'TreeNode'")
-        if node.identifier in self._nodes:
-            raise Exception(f"Node with id {node.identifier} already exists in the tree")
-        
-        # can pass either the identifier or the TreeNode as parent
-        pid = parent.identifier if isinstance(parent, TreeNode) else parent
-        if pid is None:
-            if self._root is None:
-                self._root = node.identifier
-            else:
-                raise Exception("Tree already has a root")
-        elif not self.__contains__(pid):
-            raise Exception(f"Parent node with id {pid} is not in the tree")
-        
-        # update refs
-        self._nodes.update({node.identifier: node})
-        self[pid].add_child(node._identifier)
-        node.set_parent(pid)
+    def add_node(self, node: TreeNode, parent: TreeNode = None) -> None:
+        """Adds a node to the tree, either as the root or as a child of an existing node.
 
-    def create_node(self, identifier, parent=None, data=None):
-        if identifier is None:
-            raise Exception("Must pass a valid identifier")
-        node = TreeNode(data=data, identifier=identifier)
-        self.add_node(node, parent=parent)
-        return node
-    
-    def children(self, node_identifier):
-        return self[node_identifier].children
-    
-    def remove_node(self, node_identifier):
-        # TODO
+        If a valid `parent` is provided, the `node` is added as a child of the `parent`, and the `node`'s parent is set accordingly.
+
+        Args:
+            node: The node to add to the tree.
+            parent: The parent node under which the `node` will be added.
+                If `None`, the `node` will be set as the root of the tree. Defaults to None.
+
+        Raises:
+            TypeError: If `node` is not an instance of `TreeNode`, or if `parent` is provided and is not an instance of `TreeNode`.
+            ValueError: If the `node` already exists in the tree, or if the tree already has a root when trying to add a root node.
+            LookupError: If the `parent` node is not found in the tree.
+        """
+        if not isinstance(node, TreeNode):
+            raise TypeError("'node' must be an instance of 'TreeNode'")
+        if parent and not isinstance(parent, TreeNode):
+            raise TypeError("'parent' must be an instance of 'TreeNode'")
+        if node in self._nodes:
+            raise ValueError(f"Node '{node}' already exists in the tree")
+
+        if parent is None:  # If no parent is provided, it must be the root node
+            if self._root is None:
+                self._root = node
+            else:
+                raise ValueError("Tree already has a root")
+        elif not self.__contains__(parent):
+            raise LookupError(f"Parent node '{parent}' is not in the tree")
+        else:
+            parent.add_child(node)
+            node.set_parent(parent)
+
+        self._nodes.add(node)
+
+    def create_node(self, typology: NodeType, data=None) -> TreeNode:
+        """Creates a new node with the specified typology and optional data.
+
+        Args:
+            typology: The type of the node to create. Must be a valid `NodeType`.
+            data: Data to store in the node. Defaults to None.
+
+        Raises:
+            TypeError: If `typology` is not a valid `NodeType`.
+
+        Returns:
+            The newly created node.
+        """
+        # Single Responsibility principle
+        if not isinstance(typology, NodeType):
+            raise TypeError("'typology' must be a valid node type")
+
+        return TreeNode(typology, data=data)
+
+    def children(self, node: TreeNode) -> list[TreeNode]:
+        """Returns all the children of a node."""
+        return node.get_children()
+
+    def remove_node(self, node: TreeNode) -> None:
+        # TODO: remove node from the tree, and remove all its successors (?)
         pass
--- ast/tree_node.py
+++ ast/tree_node.py
@@ -1,18 +1,44 @@
-class TreeNode:
-    def __init__(self, data=None, identifier=None):
-        self.data = data
-        self._identifier = identifier
-        self._children = []
-        self._parent_identifier = None
+from enum import Enum
 
-    def children(self):
+
+class NodeType(Enum):
+    PROGRAM = 1
+    FUNCTION = 2
+    STATEMENT = 3
+    EXPRESSION = 4
+
+
+class TreeNode:
+    def __init__(
+        self,
+        typology: NodeType,
+        data=None,  # TODO: what type of data can be?
+    ):
+        self._type = typology
+        self.data = data
+        self._parent: TreeNode = None
+        self._children: list[TreeNode] = []
+
+    def __str__(self) -> str:
+        return f"TreeNode(type={self._type}, data={self.data})"
+
+    def get_children(self) -> list["TreeNode"]:
         return self._children
-    
-    def add_child(self, child_identifier):
-        self._children.append(child_identifier)
-    
-    def remove_child(self, child_identifier):
-        self._children.remove(child_identifier)
-    
-    def set_parent(self, parent_identifier):
-        self._parent_identifier = parent_identifier
+
+    def add_child(self, child: "TreeNode") -> None:
+        self._children.append(child)
+
+    def remove_child(self, child: "TreeNode") -> None:
+        self._children.remove(child)
+
+    def set_parent(self, parent: "TreeNode") -> None:
+        self._parent = parent
+
+    def get_parent(self) -> "TreeNode":
+        return self._parent
+
+    def is_leaf(self) -> bool:
+        return len(self._children) == 0
+
+    def is_type(self, node_type: NodeType) -> bool:
+        return self._type == node_type
