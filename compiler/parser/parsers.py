d865890c487c596a0b8d309ff39c2b257c173a93
marco mariotto <mari8.marco@gmail.com>
2024-11-14 17:18:21 +0100

    refactor: added doxygen added missing type hints

--- compiler/parser/parsers.py
+++ compiler/parser/parsers.py
@@ -5,11 +5,26 @@ from compiler.parser.ast import (
     Statement,
     FunctionDefinition,
     Exp,
+    Program,
 )
 from compiler.lexer.lexer import Token
+from lib.tree.tree import Tree
 
 
 def expect_token_type(expected_type: Token.TokenType, tokens: list[Token]) -> Token:
+    """Pops the first element out of the tokens list and compares it to the expected type.
+
+    Args:
+        expected_type: The expected token type.
+        tokens: A list of tokens.
+
+    Returns:
+        The popped token.
+
+    Raises:
+        SyntaxError: if the tokens list is empty or the expected token differs from the actual token.
+    """
+
     try:
         token = tokens.pop(0)
         if token.type != expected_type:
@@ -20,6 +35,16 @@ def expect_token_type(expected_type: Token.TokenType, tokens: list[Token]) -> To
 
 
 def parse_identifier(tokens: list[Token]) -> Identifier:
+    """Parses a list of tokens representing an identifier and validates the syntax.
+
+    Args:
+        tokens: A list of tokens.
+
+    Returns:
+        AST node representing the identifier.
+    """
+
+    # example:
     # tok.type = Token.TokenType.Identifier
     # tok.value == "main"
     # -> Identifier(name="main", parent=None)
@@ -28,11 +53,29 @@ def parse_identifier(tokens: list[Token]) -> Identifier:
 
 
 def parse_statement(tokens: list[Token]) -> Statement:
-    # chapter 1
+    """Parses a list of tokens representing a statement and validates the syntax.
+
+    Args:
+        tokens: A list of tokens.
+
+    Returns:
+        AST node representing the statement.
+    """
+
+    # chapter 1: the statement is a return statement
     return parse_return_statement(tokens)
 
 
 def parse_function(tokens: list[Token]) -> FunctionDefinition:
+    """Parses a list of tokens representing a function and validates the syntax.
+
+    Args:
+        tokens: A list of tokens.
+
+    Returns:
+        AST node representing the function.
+    """
+
     # chapter 1
     # "int" <identifier> "(" "void" ")" "{" <statement> "}"
     expect_token_type(Token.TokenType.IntKeyword, tokens)
@@ -49,35 +92,62 @@ def parse_function(tokens: list[Token]) -> FunctionDefinition:
     return func
 
 
-def parse_program(tokens: list[Token]):
-    parse_function(tokens)
+def parse_program(tokens: list[Token]) -> Program:
+    """Parses a list of tokens representing a program and validates the syntax.
+
+    Args:
+        tokens: A list of tokens.
+
+    Returns:
+        AST node representing the program.
+    """
+
+    main_func = parse_function(tokens)
     # no extra junk after function
     # TODO: better!
     assert len(tokens) == 0
+    prog = Program()
+    main_func.parent = prog
+    return prog
 
 
-def parse(tokens: list[Token]):
-    parse_program(tokens)
+def parse(tokens: list[Token]) -> Tree:
+    """Parses the list of tokens that compose the program.
+    Args:
+        tokens: A list of tokens.
+
+    Returns:
+        The parse tree rooted at the program node.
+    """
+    prog = parse_program(tokens)
+    return Tree(prog)
 
 
 def parse_expression(tokens: list[Token]) -> Exp:
-    # chapter 1, constant only
+    """Parses a list of tokens representing a an expression and validates the syntax.
+
+    Args:
+        tokens: A list of tokens.
+
+    Returns:
+        AST node representing the expression.
+    """
+
+    # chapter 1: the expression is a constant
     tok = expect_token_type(Token.TokenType.Constant, tokens)
-    return Constant(None, value=tok.value)
+    return Constant(parent=None, value=tok.value)
 
 
 def parse_return_statement(tokens: list) -> Return:
     """Parses a list of tokens representing a return statement and validates the syntax.
 
     Args:
-        tokens: A list of tuples where each tuple consists of a token type and its
-                corresponding value.
+        tokens: A list of tokens.
 
     Returns:
         AST node representing the Return statement.
     """
-
-    # "return" <exp> ";"
+    # chapter 1: "return" <exp> ";"
     expect_token_type(Token.TokenType.ReturnKeyword, tokens)
     expr = parse_expression(tokens)
     expect_token_type(Token.TokenType.Semicolon, tokens)
